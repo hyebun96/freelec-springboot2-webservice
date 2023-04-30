ccebc8092a4e27a456ca7df60d396172d3ebf394
hyebun <hyebun96@naver.com>
2023-04-30 17:45:17 +0900

    - 게시글 전체 조회 추가 - SpringDataJps 에서 제공하는 메소드로 사용할 수 있지만 사용할 수 있는 메소드가 없는경우 @Query 추가하는 경우를 적용해봄 - @Transactional(readOnly = true)는 트랜잭션 범위는 유지, 조회 속도가 개선

--- src/main/java/com/bunne/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/bunne/book/springboot/domain/posts/Posts.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Posts extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(length = 500, nullable = false)
