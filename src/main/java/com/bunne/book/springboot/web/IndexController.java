f175c8627d1eace2e8d8d0f78e7097f8a926fb01
hyebun <hyebun96@naver.com>
2023-05-04 15:11:26 +0900

    - 로그인 테스트 구현

--- src/main/java/com/bunne/book/springboot/web/IndexController.java
+++ src/main/java/com/bunne/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.bunne.book.springboot.web;
 
+import com.bunne.book.springboot.config.auth.dto.SessionUser;
 import com.bunne.book.springboot.service.posts.PostsService;
 import com.bunne.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
