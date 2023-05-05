176ab848de0a099d435a6db6959b43b75b5202f2
hyebun <hyebun96@naver.com>
2023-05-05 20:28:32 +0900

    - IndexController.java 세션 값을 가져오는 부분 어노테이션(사용자 어노테이션 @LoginUser)으로 개선 - LoginUserArgumentResolver.java : HandelerMethodArgumentResolver의 구현체(조건에 맞는 메소드가 있으면 구현체가 지정한 값으로 해당 메소드의 파라미터로 넘기는 기능) - WebConfig.java : LoginUserArgumentResolver가 스프링에서 인식될 수 있도록 함

--- src/main/java/com/bunne/book/springboot/web/IndexController.java
+++ src/main/java/com/bunne/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.bunne.book.springboot.web;
 
+import com.bunne.book.springboot.config.auth.LoginUser;
 import com.bunne.book.springboot.config.auth.dto.SessionUser;
 import com.bunne.book.springboot.service.posts.PostsService;
 import com.bunne.book.springboot.web.dto.PostsResponseDto;
@@ -17,13 +18,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
